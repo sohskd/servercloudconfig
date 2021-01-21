8aaaa87c5ac6a4bd73aa44c5c651bf7fcec0ae6d
Desmond <desmond.sohsk@shopee.com>
2021-01-21 16:18:37 +0800

    another branch

--- config-client/src/main/java/com/profesorp/configclient/ConfigServiceController.java
+++ config-client/src/main/java/com/profesorp/configclient/ConfigServiceController.java
@@ -13,32 +13,32 @@ public class ConfigServiceController {
 	@Value("${valores.valor_fijo}")
 	String valorFijo;
 	
-	@Value("${valores.valor_funcion}") 
-	String valorFuncion;
-	
-	@Autowired
-	private Configuration configuration;
-
-	@Autowired
-	private ConfigurationData configurationData;
-
-	@GetMapping("/limites")
-	public BeanConfiguration getConfiguracion()
-	{
-		return new BeanConfiguration(configuration.getMinimum(),
-				configuration.getMaximum(),valorFijo,valorFuncion);
-	}
-	
-	
-	@GetMapping("/refrescado")
-	public BeanConfiguration getConfiguracionRefrescada(@Value("${valores.valor_funcion}") String valorFuncion)
-	{
-		return new BeanConfiguration(configuration.getMinimum(),
-				configuration.getMaximum(),valorFijo,valorFuncion);
-	}
-	@GetMapping("/datos")
-	public ConfigurationData retrieveDatosFromConfigurations() {
-		return configurationData;
-	}
-	
+//	@Value("${valores.valor_funcion}")
+//	String valorFuncion;
+//
+//	@Autowired
+//	private Configuration configuration;
+//
+//	@Autowired
+//	private ConfigurationData configurationData;
+//
+//	@GetMapping("/limites")
+//	public BeanConfiguration getConfiguracion()
+//	{
+//		return new BeanConfiguration(configuration.getMinimum(),
+//				configuration.getMaximum(),valorFijo,valorFuncion);
+//	}
+//
+//
+//	@GetMapping("/refrescado")
+//	public BeanConfiguration getConfiguracionRefrescada(@Value("${valores.valor_funcion}") String valorFuncion)
+//	{
+//		return new BeanConfiguration(configuration.getMinimum(),
+//				configuration.getMaximum(),valorFijo,valorFuncion);
+//	}
+//	@GetMapping("/datos")
+//	public ConfigurationData retrieveDatosFromConfigurations() {
+//		return configurationData;
+//	}
+//
 }
--- config-client/src/main/java/com/profesorp/configclient/ServiceApplication.java
+++ config-client/src/main/java/com/profesorp/configclient/ServiceApplication.java
@@ -1,12 +1,23 @@
 package com.profesorp.configclient;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ServiceApplication {
+public class ServiceApplication implements CommandLineRunner {
+
+	@Value("${valores.valor_fijo}")
+	String valorFijo;
 
 	public static void main(String[] args) {
 		SpringApplication.run(ServiceApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		System.out.println("hi");
+		System.out.println(valorFijo);
 	}
 }
